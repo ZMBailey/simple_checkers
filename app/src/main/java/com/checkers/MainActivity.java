219d505992ae666e7a57bd6f61905649cff54cca
Zander Bailey <zbailey656@gmail.com>
2020-02-23 13:36:39 -0800

    spaces now have a border

--- app/src/main/java/com/checkers/MainActivity.java
+++ app/src/main/java/com/checkers/MainActivity.java
@@ -3,6 +3,7 @@ package com.checkers;
 import androidx.appcompat.app.AppCompatActivity;
 //import android.content.DialogInterface;
 import android.content.res.Configuration;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.graphics.Point;
 //import android.support.v7.app.AppCompatActivity;
@@ -10,28 +11,23 @@ import android.graphics.Point;
 //import android.view.Gravity;
 import android.view.View;
 //import android.view.ViewGroup;
-import android.widget.Button;
 import androidx.gridlayout.widget.GridLayout;
 //import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 //import java.util.Arrays;
 //import java.util.Locale;
 
-import static android.R.drawable.alert_light_frame;
 import static android.R.color.holo_blue_light;
-
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button spaces[][];
+    public View spaces[][];
     public int side = 8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        spaces = new Button[side][side];
+        spaces = new View[side][side];
 
         setContentView(R.layout.activity_main);
 
@@ -76,17 +72,25 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private GradientDrawable getBorder(int bg,int line){
+        GradientDrawable border = new GradientDrawable();
+        border.setColor(bg);
+        border.setStroke(1,line);
+        return border;
+    }
+
     //configures buttons for a square layout
     private void setButtons_Square(GridLayout grid, int w){
 
         for(int row=0; row< side; row++){
             for(int col=0; col<side; col++) {
                 String id = Integer.toString(row)+Integer.toString(col);
-                spaces[row][col] = new Button(this);
+                spaces[row][col] = new View(this);
                 //spaces[row][col].setOnClickListener(bh);
                 spaces[row][col].setId(View.generateViewId());
-                spaces[row][col].setBackground(getResources().getDrawable(alert_light_frame,null));
-                spaces[row][col].setBackgroundTintList(getResources().getColorStateList(android.R.color.holo_blue_light));
+                spaces[row][col].setBackground(getResources().getDrawable(holo_blue_light,null));
+                spaces[row][col].setBackground(getResources().getDrawable(R.drawable.white_space, null));
+                //spaces[row][col].setBackgroundTintList(getResources().getColorStateList(android.R.color.holo_blue_light));
                 spaces[row][col].setId(Integer.parseInt(id));
                 grid.addView(spaces[row][col],w,w);
             }
