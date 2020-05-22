bd51443e441ba0c25e41aad61a90d8359961cef5
Zander Bailey <zbailey656@gmail.com>
2020-05-22 12:08:32 -0700

    start re structuring Piece class fo easier adaptation to otehr game modes

--- app/src/main/java/com/checkers/Piece.java
+++ app/src/main/java/com/checkers/Piece.java
@@ -1,28 +1,21 @@
 package com.checkers;
 
-public class Piece {
+import java.util.ArrayList;
+
+public abstract class Piece {
     public String color;
-    public String rank;
-    public int col;
-    public int row;
     public boolean isTaken;
 
-    public Piece(String color, String rank, int r, int c, boolean taken){
+    public Piece(String color, boolean taken){
             this.color = color;
-            this.rank = rank;
-            this.row = r;
-            this.col = c;
             this.isTaken = taken;
         }
 
-    public void setLocation(int r, int c){
-        row = r;
-        col = c;
-    }
-
     public void setTaken(){
         isTaken = true;
-        row = -1;
-        col = -1;
     }
+
+    public abstract Boolean isValidMove(Move m);
+
+    public abstract ArrayList<Move> getMoveList(int r, int c);
 }
