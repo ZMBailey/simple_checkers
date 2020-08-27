eb28c72f19291b4b28f41f492364b26483704d4b
Zander Bailey <zbailey656@gmail.com>
2020-08-26 22:32:37 -0700

    Add basic logic for Bishop movement

--- app/src/main/java/com/checkers/ChessBishop.java
+++ app/src/main/java/com/checkers/ChessBishop.java
@@ -14,8 +14,98 @@ public class ChessBishop extends Piece {
         return null;
     }
 
+    private ArrayList<Move> checkDirection(int r, int c, Piece [][] pieces, String dir){
+
+        ArrayList<Move> valid = new ArrayList<>();
+
+        int[] m2 = new int[2];
+        int pos = 0;
+        Boolean increment = isInc(dir);
+
+        switch (dir) {
+            case "Left":
+                if (c < 1) {
+                    return valid;
+                }
+
+                m2[1] = c - 1;
+                pos = 1;
+                break;
+            case "Right":
+                if(c>6){
+                    return valid;
+                }
+
+                m2[1] = c+1;
+                pos = 1;
+                break;
+            case "Up":
+                if(r>6){
+                    return valid;
+                }
+
+                m2[0] = r+1;
+                pos = 0;
+                break;
+            case "Down":
+                if(r<1){
+                    return valid;
+                }
+
+                m2[0] = r-1;
+                pos = 0;
+                break;
+        }
+
+        while(checkEdge(m2[pos],dir)){
+            Move m = new Move(r,c,m2[0],m2[1],false);
+            if(isValidMove(m, pieces[m.r2][m.c2])){
+                if(isEnemy(pieces[m.r2][m.c2])){
+                    m.isJump = true;
+                    valid.add(m);
+                    return valid;
+                }
+                valid.add(m);
+            }
+            if(increment){
+                m2[pos]++;
+            }else{
+                m2[pos]--;
+            }
+        }
+
+        return valid;
+    }
+
+    private Boolean isInc(String dir){
+        if(dir.equals("Up") || dir.equals("Right")) {
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    private Boolean checkEdge(int pos, String dir){
+        if(dir.equals("Left") || dir.equals("Down")){
+            return pos>=0;
+        }else if(dir.equals("Right") || dir.equals("Up")){
+            return pos<=7;
+        }else{
+            return false;
+        }
+    }
+
+    private Boolean isEnemy(Piece p){
+        if(p == null){
+            return false;
+        }
+
+        return !color.equals(p.color);
+    }
+
     @Override
     public ArrayList<Move> getMoveList(int r, int c, Piece [][] pieces) {
         return null;
     }
+
 }
